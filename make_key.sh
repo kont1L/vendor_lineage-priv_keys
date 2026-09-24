c7149959fd6f0d2c411edc878f3c1a9121884228
Hendry <hendry9827agmail.com>
2026-09-24 19:50:54 +0700

    Rebrand evolution > lineage

--- make_key.sh
+++ make_key.sh
@@ -6,4 +6,4 @@
 set -u
 bash <(sed "s/2048/${2:-2048}/;/Enter password/,+1d" ../../../development/tools/make_key) \
     $1 \
-    '/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'
+    '/C=ID/ST=Jakarta/L=Monas Tower/O=Prabowo/OU=Prabowo/CN=Prabowo/emailAddress=prabowo@gmail.com'
